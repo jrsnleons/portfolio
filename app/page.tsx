23db66506c2b26e6e5c224f4dd16ddbfd2284809
jello. <67309482+jrsnleons@users.noreply.github.com>
2024-09-03 01:08:16 +0800

    add shadcn and technologies section (broken scroll)

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Hero from "@/components/Hero";
 import Image from "next/image";
+import Technologies from "@/components/Technologies";
 
 export default function Home() {
     return (
         <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10px ">
             <div className="w-full">
                 <Hero></Hero>
+            </div>
+            <div className="w-full">
+                <Technologies></Technologies>
             </div>
         </main>
     );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,8 @@ const {
     default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
 
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
@@ -17,11 +19,51 @@ module.exports = {
     ],
     darkMode: "class",
     theme: {
+        container: {
+            center: true,
+            padding: "2rem",
+            screens: {
+                "2xl": "1400px",
+            },
+        },
         extend: {
             colors: {
                 black: {
                     DEFAULT: "#000",
                     100: "#000319",
+                },
+                border: "hsl(var(--border))",
+                input: "hsl(var(--input))",
+                ring: "hsl(var(--ring))",
+                background: "hsl(var(--background))",
+                foreground: "hsl(var(--foreground))",
+                primary: {
+                    DEFAULT: "hsl(var(--primary))",
+                    foreground: "hsl(var(--primary-foreground))",
+                },
+                secondary: {
+                    DEFAULT: "hsl(var(--secondary))",
+                    foreground: "hsl(var(--secondary-foreground))",
+                },
+                destructive: {
+                    DEFAULT: "hsl(var(--destructive))",
+                    foreground: "hsl(var(--destructive-foreground))",
+                },
+                muted: {
+                    DEFAULT: "hsl(var(--muted))",
+                    foreground: "hsl(var(--muted-foreground))",
+                },
+                accent: {
+                    DEFAULT: "hsl(var(--accent))",
+                    foreground: "hsl(var(--accent-foreground))",
+                },
+                popover: {
+                    DEFAULT: "hsl(var(--popover))",
+                    foreground: "hsl(var(--popover-foreground))",
+                },
+                card: {
+                    DEFAULT: "hsl(var(--card))",
+                    foreground: "hsl(var(--card-foreground))",
                 },
             },
             animation: {
@@ -31,8 +73,31 @@ module.exports = {
                 fourth: "moveHorizontal 40s ease infinite",
                 fifth: "moveInCircle 20s ease infinite",
                 spotlight: "spotlight 2s ease .75s 1 forwards",
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
+                "infinite-scroll": "infinite-scroll 25s linear infinite",
+            },
+            borderRadius: {
+                lg: `var(--radius)`,
+                md: `calc(var(--radius) - 2px)`,
+                sm: "calc(var(--radius) - 4px)",
+            },
+            fontFamily: {
+                sans: ["var(--font-sans)", ...fontFamily.sans],
             },
             keyframes: {
+                "infinite-scroll": {
+                    from: { transform: "translateX(0)" },
+                    to: { transform: "translateX(-100%)" },
+                },
+                "accordion-down": {
+                    from: { height: "0" },
+                    to: { height: "var(--radix-accordion-content-height)" },
+                },
+                "accordion-up": {
+                    from: { height: "var(--radix-accordion-content-height)" },
+                    to: { height: "0" },
+                },
                 spotlight: {
                     "0%": {
                         opacity: 0,
@@ -80,6 +145,7 @@ module.exports = {
         },
     },
     plugins: [
+        require("tailwindcss-animate"),
         addVariablesForColors,
         function ({ matchUtilities, theme }: any) {
             matchUtilities(
